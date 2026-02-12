3e2ece311b43db0bd51c373b0a6cc611571a4c43
Paila Murali Madhav <muralipaila1313@gmail.com>
2026-02-12 12:15:25 +0530

    Enhance student update options in App
    Refactored student update functionality to allow updating name and age separately. Added options for updating student details in the main menu.

--- HibernateDemo/src/main/java/org/example/App.java
+++ HibernateDemo/src/main/java/org/example/App.java
@@ -20,7 +20,7 @@ public class App {
                         .buildSessionFactory();
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter 1.to create student\n 2.to read\n 3. to update student \n 4. to delete student\n 5. to exit");
-        System.out.print("Enter a choice: ");
+        System.out.println("Enter a choice: ");
         int choice = sc.nextInt();
         switch(choice) {
         case 1:
@@ -34,13 +34,33 @@ public class App {
         	readStudent();
         	break;
         case 3:
+        	System.out.println("1.to update name\n 2. to update Age\n 3. to update name and age ");
+        	System.out.println("Enter Option: ");
+        	int option = sc.nextInt();
+        	System.out.println("");
         	System.out.println("Enter id: ");
         	int id = sc.nextInt();
-        	System.out.print("Enter name: ");
-        	String newName= sc.next();
-        	System.out.print("Enter age: ");
-        	int newAge = sc.nextInt();        	
-        	updateStudent(id,newName,newAge);
+        	switch(option) {
+        	case 1:
+            	System.out.println("Enter New name: ");
+            	String newName= sc.next();
+            	updateName(id, newName);
+            	break;
+        	case 2:
+        		System.out.println("Enter new Age: ");
+        		int newAge = sc.nextInt();
+        		updateAge(id, newAge);
+        		break;
+        	case 3:
+            	System.out.println("Enter New name: ");
+            	String newName1= sc.next();
+        		System.out.println("Enter new Age: ");
+        		int newAge1 = sc.nextInt();
+        		updateStudent(id, newName1, newAge1);
+        		break;
+        	default:
+        		System.out.println("Invalid Input!");
+        	}
         	break;
         case 4:
         	System.out.println("Enter id: ");
@@ -55,18 +75,6 @@ public class App {
         default:
         	System.out.println("Invalid!");	
         }
-//        System.out.println("Enter name: ");
-//        String name = sc.next();
-//        System.out.println("Enter age: ");
-//        int age= sc.nextInt();
-//        createStudent(name,age);
-//        readStudent();
-//        
-//        updateStudent(3, "raju",30);
-////        updateStudent(4, "Bhaai");
-//        readStudent();
-//
-////        deleteStudent(4);
         factory.close();
     }
 
@@ -96,21 +104,37 @@ public class App {
 
     	session.close();
     }
-//    public static void updateStudent(int id,String newName) {
-//    	Session session = factory.openSession();
-//    	Transaction t = session.beginTransaction();
-//    	
-//    	Student s = session.get(Student.class,id);
-//   if(s != null) {
-//	   s.setName(newName);
-//	   t.commit();
-//	   System.out.println("Student updated");
-//   }else {
-//	   System.out.println("student not found");
-//	   t.rollback();
-//   }
-//   session.close();
-//    }
+    public static void updateName(int id,String newName) {
+    	Session session = factory.openSession();
+    	Transaction t = session.beginTransaction();
+    	
+    	Student s = session.get(Student.class,id);
+   if(s != null) {
+	   s.setName(newName);
+	   t.commit();
+	   System.out.println("Name updated");
+   }else {
+	   System.out.println("student not found");
+	   t.rollback();
+   }
+   session.close();
+    }
+    public static void updateAge(int id,int newAge) {
+    	Session session = factory.openSession();
+    	Transaction t = session.beginTransaction();
+    	Student s = session.get(Student.class, id);
+    	if(s!=null) {
+    		s.setAge(newAge);
+    		t.commit();
+    		System.out.println("Age updated");
+    		
+    	}
+    	else {
+    		System.out.println("Student not found");
+    		t.rollback();
+    	}
+    	session.close();
+    }
     public static void updateStudent(int  id,String newName,int newAge) {
     	Session session = factory.openSession();
     	Transaction t = session.beginTransaction();
